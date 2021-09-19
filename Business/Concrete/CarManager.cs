621d374d4ef9aa8cc8dd0fc8a6637b4452a5297f
Ali Kemal Çelik <alikemalcelik42@gmail.com>
2021-09-19 14:46:38 +0300

    19.09.2021

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,10 +1,14 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results.Abstract;
 using Core.Utilities.Results.Concrete;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -20,10 +24,9 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
-            if (car.DailyPrice <= 0)
-                return new ErrorResult(Messages.InvalidDailyPrice);
             _carDal.Add(car);
             return new SuccessResult(Messages.CarAdded);
         }
