6ded5f4a202a5e00fe36d3c2fdd5120ace5b9ca3
alikemalcelik42 <alikemalcelik42@gmail.com>
2021-12-15 18:01:07 +0300

    15.12.2021

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -2,6 +2,7 @@
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Caching;
+using Core.Aspects.Autofac.IpBan;
 using Core.Aspects.Autofac.Logging;
 using Core.Aspects.Autofac.Performance;
 using Core.Aspects.Autofac.Secure;
@@ -46,7 +47,7 @@ namespace Business.Concrete
             return new SuccessResult(Messages.CarDeleted);
         }
 
-
+        [IpBanAspect]
         [CacheAspect]
         public IDataResult<List<Car>> GetAll()
         {
